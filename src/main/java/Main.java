ec412c13d070b507eade411359037a92ed19dd36
Amol <amolwani9192@gmail.com>
2022-03-26 12:15:08 +0530

    inital commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Started..");
+        System.out.println(addNumbers(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+    }
+
+    public static int addNumbers(int i, int j){
+        return i+j;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void testAddNumbers(){
+        Assert.assertEquals(3, Main.addNumbers(1, 2));
+    }
+
+//    @Test
+//    public void testAddNumbersPass(){
+//        Assert.assertEquals(4, Main.addNumbers(1, 2));
+//    }
